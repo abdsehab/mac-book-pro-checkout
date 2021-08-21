0feed849512cf0a6db8ecbf063e7e49d95d09c7a
Sehab-sketch <abdullahalsehab2017@gmail.com>
2021-08-21 14:16:54 +0600

    Calculation

--- main.js
+++ main.js
@@ -4,32 +4,83 @@ function CostAdd(option1,option2,ItemCost, Price,Option3){
     const Choice3 = document.getElementById(Option3);
     const TargetInnerText = event.target.innerText;
     const ExtraCost = document.getElementById(ItemCost);
-
+   
     if (TargetInnerText == Choice1.innerText) {
-        ExtraCost.innerText = 0;
+        ExtraCost.innerText = 0;   
     }
     else if (TargetInnerText == Choice2.innerText) {
-        ExtraCost.innerText = Price;
+        ExtraCost.innerText = Price;  
     }
     else {
-        ExtraCost.innerText = Price*2;
+        ExtraCost.innerText = Price * 2; 
     }
 }
 
+const baseprice = document.getElementById('base-price');
+const basepriceValue = baseprice.innerText;
+const TotalValue = document.getElementById('total-price');
+const GrantTotal = document.getElementById('grand-total');
 document.getElementById('memory').addEventListener('click', function (event) { 
     CostAdd('memory-8gb', 'memory-16gb', 'memory-cost', 200)
+    const StorageCost = document.getElementById('storage-cost').innerText;
     const MemoryCost = document.getElementById('memory-cost').innerText;
-    console.log(MemoryCost)
+    const DeliveryCost = document.getElementById('Delivary-cost').innerText;
+    
+ function Total() {
+        let sum = parseInt(basepriceValue)  + parseInt(MemoryCost) + parseInt(StorageCost) + parseInt(DeliveryCost);
+        return sum
+    }
+
+    TotalValue.innerText = Total();
+    GrantTotal.innerText = TotalValue.innerText;
 })
 
 document.getElementById('storage').addEventListener('click', function (event) {
     CostAdd('SSD-256GB','SSD-512GB','storage-cost',300,'SSD-1TB')
+    const StorageCost = document.getElementById('storage-cost').innerText;
+    const MemoryCost = document.getElementById('memory-cost').innerText;
+    const DeliveryCost = document.getElementById('Delivary-cost').innerText;
+    
+
+    function Total() {
+        let sum = parseInt(basepriceValue)  + parseInt(MemoryCost) + parseInt(StorageCost) + parseInt(DeliveryCost);
+        return sum
+    }
+
+    TotalValue.innerText = Total();
+    GrantTotal.innerText = TotalValue.innerText;
 })
 
 document.getElementById('devilary-option').addEventListener('click', function (event) {
-    CostAdd('free-delivary','paid-delivary','Delivary-cost',50)
+    CostAdd('free-delivary', 'paid-delivary', 'Delivary-cost', 50)
+    const StorageCost = document.getElementById('storage-cost').innerText;
+    const MemoryCost = document.getElementById('memory-cost').innerText;
+    const DeliveryCost = document.getElementById('Delivary-cost').innerText;
+    
+     function Total() {
+        let sum = parseInt(basepriceValue)  + parseInt(MemoryCost) + parseInt(StorageCost) + parseInt(DeliveryCost);
+        return sum
+    }
+
+    TotalValue.innerText = Total();
+    GrantTotal.innerText = TotalValue.innerText;
 })
 
-const TotalPrice = document.getElementById('total-price');
+//     const StorageCost = document.getElementById('storage-cost').innerText;
+//     const MemoryCost = document.getElementById('memory-cost').innerText;
+//     const DeliveryCost = document.getElementById('Delivary-cost').innerText;
 
-TotalPrice.innerText = parseInt(MemoryCost.innerText)
+// document.getElementById('hudai').addEventListener('click', function () {
+//     function Total() {
+//         let sum = parseInt(basepriceValue)  + parseInt(MemoryCost) + parseInt(StorageCost) + parseInt(DeliveryCost);
+//         return sum
+//     }
+
+//     TotalValue.innerText = Total()
+// })
+
+
+// Coupon Code Option
+
+
+    
